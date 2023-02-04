bdb76f46014f3f6d0e0be11c1c16b530336d292e
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-02-05 01:43:51 +0330

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export default {
     SITE_NAME: "باونتد موویز",
-    API_URL: 'https://streamapi.irn.workers.dev',
+    API_URL: 'https://stapijs.netlify.app',
     TMDB_IMAGE_URL: 'https://tmdb.irn.workers.dev/t/p',
 }
