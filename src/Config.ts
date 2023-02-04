8fbd31980d65c7c85baee26953dc3ac3fdd147b5
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-02-05 02:01:07 +0330

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export default {
     SITE_NAME: "باونتد موویز",
-    API_URL: 'https://api.ripper.lol',
+    API_URL: 'https://streamapi.irn.workers.dev',
     TMDB_IMAGE_URL: 'https://tmdb.irn.workers.dev/t/p',
 }
